51dc430d9882a58623067bf7aebc90f9958cec7c
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2024-10-09 15:25:50 +0000

    Minor changes

--- scanner/main.py
+++ scanner/main.py
@@ -16,13 +16,18 @@ class Requirements(BaseModel):
 @app.post("/scanner")
 async def lightScan(item: Requirements):
     item_dict = item.model_dump()
+    url = item_dict["url"].lower()
+    mode = item_dict['mode'].lower()
     if item_dict['url'] == "":
         return {"response": "Please provide, URL!"}
     if item_dict['mode'] == "":
         return {"response": "please provide mode"}
-    # return item_dict['url']
-    scan = await scanModules(item_dict["url"])
-    return scan
+    if mode == "light":
+        scan = await scanModules(item_dict["url"])
+        return [scan]
+    if mode != "light":
+        return [{"response": "Other modes are on development!"}]
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
